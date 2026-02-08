a1c5a116065a09a6a2a7db5c50fa4fbb9d569326
s-sreedhar <s.sreedhargoud@gmail.com>
2026-02-08 19:30:50 +0530

    fix: update migration index

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,12 +1,6 @@
-import * as migration_20260208_134618_initial_schema from './20260208_134618_initial_schema';
 import * as migration_20260208_135414_initial_schema from './20260208_135414_initial_schema';
 
 export const migrations = [
-  {
-    up: migration_20260208_134618_initial_schema.up,
-    down: migration_20260208_134618_initial_schema.down,
-    name: '20260208_134618_initial_schema',
-  },
   {
     up: migration_20260208_135414_initial_schema.up,
     down: migration_20260208_135414_initial_schema.down,
