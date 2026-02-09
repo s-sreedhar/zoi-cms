e2e62c2c8fa24a7d01f945718512d4daabeaa939
s-sreedhar <s.sreedhargoud@gmail.com>
2026-02-09 22:04:37 +0530

    Fix deployment: Use absolute schema sync only

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,7 +1,5 @@
 import * as migration_20260208_135414_initial_schema from './20260208_135414_initial_schema';
 import * as migration_20260209_000001_sync_production_schema from './20260209_000001_sync_production_schema';
-import * as migration_20260209_000002_full_schema_reconstruction from './20260209_000002_full_schema_reconstruction';
-import * as migration_20260209_000003_complete_schema_reconstruction from './20260209_000003_complete_schema_reconstruction';
 import * as migration_20260209_000004_absolute_schema_sync from './20260209_000004_absolute_schema_sync';
 
 export const migrations = [
@@ -14,16 +12,6 @@ export const migrations = [
     up: migration_20260209_000001_sync_production_schema.up,
     down: migration_20260209_000001_sync_production_schema.down,
     name: '20260209_000001_sync_production_schema'
-  },
-  {
-    up: migration_20260209_000002_full_schema_reconstruction.up,
-    down: migration_20260209_000002_full_schema_reconstruction.down,
-    name: '20260209_000002_full_schema_reconstruction'
-  },
-  {
-    up: migration_20260209_000003_complete_schema_reconstruction.up,
-    down: migration_20260209_000003_complete_schema_reconstruction.down,
-    name: '20260209_000003_complete_schema_reconstruction'
   },
   {
     up: migration_20260209_000004_absolute_schema_sync.up,
