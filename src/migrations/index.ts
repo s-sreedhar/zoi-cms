06e1312bbc5bcb9ebe694e76b524f289ffa48563
s-sreedhar <s.sreedhargoud@gmail.com>
2026-02-14 12:58:19 +0530

    fix: Consolidated all migrations into single idempotent state file

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,46 +1,9 @@
-import * as migration_20260210_000000_master from './20260210_000000_master';
-import * as migration_20260210_122125_fix_lesson_schema_debug_v3 from './20260210_122125_fix_lesson_schema_debug_v3';
-import * as migration_20260210_122731_fix_missing_collections from './20260210_122731_fix_missing_collections';
-import * as migration_20260210_180420_safe from './20260210_180420_safe';
-import * as migration_20260211_155246_add_google_auth_fields from './20260211_155246_add_google_auth_fields';
-import * as migration_20260211_180430_add_streak_field from './20260211_180430_add_streak_field';
-import * as migration_20260214_consolidated_lexical_fix from './20260214_consolidated_lexical_fix';
+import * as migration_20260214_000000_consolidated_state from './20260214_000000_consolidated_state';
 
 export const migrations = [
   {
-    up: migration_20260210_000000_master.up,
-    down: migration_20260210_000000_master.down,
-    name: '20260210_000000_master',
-  },
-  {
-    up: migration_20260210_122125_fix_lesson_schema_debug_v3.up,
-    down: migration_20260210_122125_fix_lesson_schema_debug_v3.down,
-    name: '20260210_122125_fix_lesson_schema_debug_v3',
-  },
-  {
-    up: migration_20260210_122731_fix_missing_collections.up,
-    down: migration_20260210_122731_fix_missing_collections.down,
-    name: '20260210_122731_fix_missing_collections',
-  },
-  {
-    up: migration_20260210_180420_safe.up,
-    down: migration_20260210_180420_safe.down,
-    name: '20260210_180420_safe',
-  },
-  {
-    up: migration_20260211_155246_add_google_auth_fields.up,
-    down: migration_20260211_155246_add_google_auth_fields.down,
-    name: '20260211_155246_add_google_auth_fields',
-  },
-  {
-    up: migration_20260211_180430_add_streak_field.up,
-    down: migration_20260211_180430_add_streak_field.down,
-    name: '20260211_180430_add_streak_field',
-  },
-  {
-    up: migration_20260214_consolidated_lexical_fix.up,
-    down: migration_20260214_consolidated_lexical_fix.down,
-    name: '20260214_consolidated_lexical_fix'
+    up: migration_20260214_000000_consolidated_state.up,
+    down: migration_20260214_000000_consolidated_state.down,
+    name: '20260214_000000_consolidated_state'
   },
 ];
-
