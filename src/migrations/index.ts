2b1c0b684812672cb59dfa2687013611f20e35ad
s-sreedhar <s.sreedhargoud@gmail.com>
2026-02-15 21:18:55 +0530

    fixed workshops issue

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260214_000000_consolidated_state from './20260214_000000_consolidated_state';
 import * as migration_20260215_150000_schema_v2 from './20260215_150000_schema_v2';
+import * as migration_20260215_211500_schema_v3 from './20260215_211500_schema_v3';
 
 export const migrations = [
   {
@@ -11,5 +12,10 @@ export const migrations = [
     up: migration_20260215_150000_schema_v2.up,
     down: migration_20260215_150000_schema_v2.down,
     name: '20260215_150000_schema_v2',
+  },
+  {
+    up: migration_20260215_211500_schema_v3.up,
+    down: migration_20260215_211500_schema_v3.down,
+    name: '20260215_211500_schema_v3',
   },
 ];
